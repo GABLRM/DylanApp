c5c496bee48981982a6b1baf96efb9adb654820e
RoxanneRlld <roxanne.roulland@ynov.com>
2023-11-28 09:40:41 +0100

    create first version of notification component

--- DylanApp/App.tsx
+++ DylanApp/App.tsx
@@ -1,18 +1,42 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Onboarding } from './components/onboarding/Onboarding';
 import { colors } from './assets/Colors';
+import Notification from "./components/notificationsComponent";
 
 export default function App() {
+  const [visible, setVisible] = useState(true);
+  const [notificationMessage, setNotificationMessage] = useState("");
+
+  const handleShowNotification = () => {
+    setNotificationMessage("Ceci est un message de notification !");
+  };
+
+  const handleCloseNotification = () => {
+    setVisible(false);
+  };
+
   return (
-    <View style={styles.body}>
+    <View style={styles.container}>
       <Onboarding />
+      <View style={styles.container}>
+        <Text>coucou</Text>
+        <StatusBar style="auto" />
+        <TouchableOpacity onPress={handleShowNotification}>
+          <Notification
+            message={notificationMessage}
+            visible={visible}
+            onClose={handleCloseNotification}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  body: {
+  container: {
     flex: 1,
     backgroundColor: colors.background,
     alignItems: 'center',
