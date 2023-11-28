129b6658a2af0016932eddebe0c34a3503ecd3e6
RoxanneRlld <roxanne.roulland@ynov.com>
2023-11-28 09:40:41 +0100

    create first version of notification component

--- DylanApp/App.tsx
+++ DylanApp/App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Notification from "./components/notificationsComponent";
 
 export default function App() {
+  const [visible, setVisible] = useState(true);
+  const [notificationMessage, setNotificationMessage] = useState("");
+
+  const handleShowNotification = () => {
+    setNotificationMessage("Ceci est un message de notification !");
+  };
+
+  const handleCloseNotification = () => {
+    setVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>coucou</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={handleShowNotification}>
+        <Notification
+          message={notificationMessage}
+          visible={visible}
+          onClose={handleCloseNotification}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- DylanApp/components/fullButtonComponent.tsx
+++ DylanApp/components/fullButtonComponent.tsx
@@ -1,40 +1,40 @@
-import React from "react";
-import { StyleSheet, View, TouchableOpacity, Alert, Text } from "react-native";
-import { LinearGradient } from 'expo-linear-gradient';
+// import React from "react";
+// import { StyleSheet, View, TouchableOpacity, Alert, Text } from "react-native";
+// import { LinearGradient } from 'expo-linear-gradient';
 
-type Props = {
-    title: string
-}
-const FullButtonComponent = (props: Props) => {
-    return (
-        <View>
-            <LinearGradient
-                start={{ x: 0.1, y: 0.7 }} end={{ x: 1.2, y: 0.5 }}
-                locations={[0.1, 0.9]}
-                colors={['#4A90E2', '#28A544']} style={styles.button}>
-                <TouchableOpacity onPress={() => Alert.alert('Simple Button pressed')}>
-                    <Text style={styles.textButton}>{props.title}</Text>
-                </TouchableOpacity>
-            </LinearGradient>
-        </View >
-    );
-}
+// type Props = {
+//     title: string
+// }
+// const FullButtonComponent = (props: Props) => {
+//     return (
+//         <View>
+//             <LinearGradient
+//                 start={{ x: 0.1, y: 0.7 }} end={{ x: 1.2, y: 0.5 }}
+//                 locations={[0.1, 0.9]}
+//                 colors={['#4A90E2', '#28A544']} style={styles.button}>
+//                 <TouchableOpacity onPress={() => Alert.alert('Simple Button pressed')}>
+//                     <Text style={styles.textButton}>{props.title}</Text>
+//                 </TouchableOpacity>
+//             </LinearGradient>
+//         </View >
+//     );
+// }
 
-const styles = StyleSheet.create({
-    button: {
-        width: 300,
-        height: 45,
-        backgroundColor: '#ff5733',
-        borderRadius: 15,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    textButton: {
-        color: '#fff',
-        fontSize: 20,
-        fontWeight: "600",
-    }
+// const styles = StyleSheet.create({
+//     button: {
+//         width: 300,
+//         height: 45,
+//         backgroundColor: '#ff5733',
+//         borderRadius: 15,
+//         justifyContent: "center",
+//         alignItems: "center",
+//     },
+//     textButton: {
+//         color: '#fff',
+//         fontSize: 20,
+//         fontWeight: "600",
+//     }
 
-})
+// })
 
-export default FullButtonComponent;
+// export default FullButtonComponent;
