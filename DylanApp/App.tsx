d9edb25784205b1954636b39edecce4c012f42dd
GABLRM <g.laroumanie@gmail.com>
2023-11-28 16:16:37 +0100

    resolving conflict

--- DylanApp/App.tsx
+++ DylanApp/App.tsx
@@ -5,6 +5,7 @@ import { colors } from "./assets/Colors"
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
+import React from 'react';
 
 const Match = () => {
   return (
